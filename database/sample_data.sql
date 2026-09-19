8707f2cc5ff537bd627913bc0f9978c8df909399
Aayushi_Sharma <aayushi.sharma7275@gmail.com>
2026-09-19 19:48:23 +0530

    Update sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -2,4 +2,7 @@ INSERT INTO accounts (account_id, name, balance)
 VALUES
 ('A001', 'Anubhuti', 10000.00),
 ('A002', 'Aayushi', 5000.00),
-('A003', 'Shriya', 3000.00);
+('A003', 'Shriya', 3000.00),
+('A004','Saloni',7000.00),
+('A005','Yashika',10000.00),
+('A006','Karan',11000.00);
