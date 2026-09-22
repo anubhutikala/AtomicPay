0811b7e109005fb66b9690186b783946777d88b5
Anubhuti Kala <anubhutikala2006@gmail.com>
2026-09-23 00:01:42 +0530

    Set up Spring Boot backend and database connection

---
+++ backend/src/test/java/com/atomicpay/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomicpay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
